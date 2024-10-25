deb0f7684dd53bf215d14c8a4930f80ca7f696d7
Bryan Escobar <lobo638yt@gmail.com>
2024-10-25 16:07:46 -0500

    version pre-alpha 0.3

--- js/cokkie.js
+++ js/cokkie.js
@@ -5,29 +5,47 @@ let multiplicador = 1;
 // CONSTANTE PARA ACTUALIZAR LA PUNTUACIÓN
 const counterElement = document.getElementById('counter');
 
-function increase() {
+
+function clicked() {
     count = count + multiplicador;
 
     // INCREMENTOS
-    if(count >= 50 ) {
+    if(count >= 100 ) {
         multiplicador = 2;
     }
-    if (count >= 100){
-        multiplicador = 10000000000000000;
-    };
+    if (count >= 200){
+        multiplicador = 4;
+    }
+    if (count >= 400){
+        multiplicador = 6;
+    }
+    if (count >= 800){
+        multiplicador = 8;
+    }
 
     // ACTUALIZACION DE MULTIPLICADOR
     document.getElementById('numero').textContent = "Tienes un x" + multiplicador;
-    function mostrarNumero() {
-        updateDisplay();
-    }
-    mostrarNumero();
 
+    // ACTUALIZACIÓN DE FASE
+    if (count >= 1 && count <100){
+        document.getElementById('fase').textContent = "MINI";
+    }else if (count >= 100 && count <200){
+        document.getElementById('fase').textContent = "BASE";
+    }else if (count >= 200 && count <350){
+        document.getElementById('fase').textContent = "KAIOKEN";
+    }else if (count >= 350){
+        document.getElementById('fase').textContent = "FALSE SUPER COOKIE";
+    }
+    
     // ACTUALIZACION DE IMAGENES
-    if (count == 100) {
-        document.getElementById('imagenx1').src = "img/galleta-tradicional-small-new.webp";
+    if (count >= 100 && count <200) {
+        document.getElementById('imagen').src = "img/galleta-base.png";
+    }else if (count >= 200 && count <350) {
+        document.getElementById('imagen').src = "img/galleta-kaioken.png";
+    }else if (count >= 350) {
+        document.getElementById('imagen').src = "img/galleta-falsesupercookie.png";
     }
-
+    
     // ACTUALIZACION DE PUNTACIÓN
     function updateDisplay() {
         counterElement.textContent = count;
@@ -36,8 +54,7 @@ function increase() {
     updateDisplay();
 }
 
-// GUARDAR PUNTUACIÓN
-
+// MODAL DE UPDATES
 window.onload = function() {
     const miModal = new bootstrap.Modal(document.getElementById('ModalUpdates'));
     miModal.show();
